9123c7fdf2d0fa389a9b4f4e81ce8ef977219521
puzzlepeaches <nicholas_anastasi@yahoo.com>
2020-11-20 07:35:16 -0600

    added external mail recon script

---
+++ mail-recon.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# A really crap script to get info on target domain mail servers
+# Usage: ./mail-recon.sh acme.com
+
+pip3 install mailspoof
+pip3 install checkdmarc
+apt install dnsutils -y
+
+domain="$1"
+
+mailspoof -d $domain -o mailspoof."$domain".json
+checkdmarc $domain -o checkdmarc."$domain".json
+dig +short $domain MX | tee digmx."$domain".txt
+dig +short $domain TXT | tee digtxt."$domain".txt
+
+for i in owa post mail mymail exchange autodiscover exch exch01 outlook
+do
+        host $i.$domain | tee -a exchange-enum."$domain".txt
+done
